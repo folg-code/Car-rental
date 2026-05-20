823953f386186f6744680a8c56574613ac82ea2b
filip <filip1folga@gmail.com>
2026-05-20 15:06:08 +0200

    CI/CD fundation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 import sys
+from pathlib import Path
 
 import environ
-from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
